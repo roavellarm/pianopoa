fc6ed46b0ff1ad733f7f4e37f89b28b58a37e358
Gustavo <gustavo.prog40@gmail.com>
2022-03-08 13:55:01 -0300

    container header test styles

--- src/__test__/HomeText.error.tsx
+++
@@ -1,9 +0,0 @@
-// import { render } from '@testing-library/react'
-
-// import HomeText from '../components/HomeText/HomeText'
-
-// describe('HomeText', () => {
-//   it('should render correctly', () => {
-//     render(<HomeText />)
-//   })
-// })
