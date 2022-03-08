80f98c416c3dee535fc46036a148d80d7fc26330
Gustavo <gustavo.prog40@gmail.com>
2022-03-08 14:03:33 -0300

    tenting header

--- src/__test__/Footer.error.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-
-import Footer from '../components/Footer/Footer'
-
-describe('Footer', () => {
-  it('should render correctly', () => {
-    render(<Footer />)
-  })
-})
