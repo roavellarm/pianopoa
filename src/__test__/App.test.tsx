6e2ec213cbdd09a13dd327c73f160d8c59471bfa
Gustavo <gustavo.prog40@gmail.com>
2022-03-10 09:33:02 -0300

    test app h1 hellow world

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,9 +1,17 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import { App } from '../App'
 
 describe('App', () => {
-  it('should render correctly', () => {
+  it('should render correctly and compare if have a text Hellow World', () => {
     render(<App />)
+  })
+
+  it('should compare if have a text Hellow World', () => {
+    render(<App />)
+
+    const getText = screen.getByText('Hellow World')
+
+    expect(getText).toBeInTheDocument()
   })
 })
