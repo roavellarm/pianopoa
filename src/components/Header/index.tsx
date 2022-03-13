6d9466acc24c87f348a3526486e08e879f6dc47e
Gustavo <gustavo.prog40@gmail.com>
2022-03-13 13:39:32 -0300

    pianopoa text

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,19 @@
 import { HeaderContainer } from './style'
+import { theme } from '../../styles/theme'
 
 function Header() {
   return (
     <HeaderContainer>
-      <h1>Header</h1>
+      <h1
+        style={{
+          fontFamily: theme.fontFamily.urbanist,
+          fontSize: '31px',
+          marginLeft: '20px',
+          letterSpacing: '15px',
+        }}
+      >
+        PIANO POA
+      </h1>
     </HeaderContainer>
   )
 }
