8ae5f28ec06a60c1c0026793054ecdeece388d3f
Gustavo <gustavo.prog40@gmail.com>
2022-03-16 12:08:00 -0300

    footer inicio

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+import { FooterContainer } from './styled-footer'
+
+function Footer() {
+  return <FooterContainer>Footer</FooterContainer>
+}
+
+export default Footer
---
+++ src/components/Footer/styled-footer.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const FooterContainer = styled.div`
+  width: 100%;
+  height: 100px;
+  background: rgba(244, 211, 194, 0.9);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-top: 45.2rem;
+  color: gray;
+`
