b1e401c634c687dc1a406c56b4743a639eada02c
Rodrigo Avellar <roavellarm@gmail.com>
2022-04-09 17:49:53 -0300

    Refactor Header to Navbar and add links actions

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 
-import { Header } from './components/Header'
+import { Navbar } from './components/Navbar'
 import { Home } from './pages/Home'
 
 export const Router = () => (
   <>
-    <Header />
+    <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
---
+++ src/components/Navbar/styles.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+import theme from '../../styles/theme'
+
+export const NavbarContainer = styled.h1`
+  display: flex;
+  position: fixed;
+  justify-content: center;
+  align-items: center;
+  height: 5.5rem;
+  width: 100%;
+  background: rgba(16, 12, 8, 0.5);
+  box-shadow: 0px 0px 25px 25px rgba(16, 12, 8, 0.5);
+  filter: brightness(77%);
+`
+export const Brand = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: ${theme.fontFamily.oswald};
+  font-size: 2.5rem;
+  white-space: nowrap;
+  margin-right: 1rem;
+  letter-spacing: 0.5rem;
+  line-height: 160%;
+  text-shadow: 4px 4px 8px rgba(0, 0, 0, 0.51);
+  font-weight: 400;
+  color: rgba(239, 239, 235, 1);
+  cursor: pointer;
+`
+export const LinksWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-left: 1rem;
+
+  @media screen and (max-width: 750px) {
+    display: none;
+  }
+`
+export const NavLink = styled.span<{ isSelected: boolean }>`
+  margin: 0 1.35rem;
+  font-family: ${theme.fontFamily.urbanist};
+  font-size: 1.5rem;
+  line-height: 160%;
+  color: ${({ isSelected }) => (isSelected ? '#90948D' : 'rgba(244, 211, 194, 1)')};
+  text-shadow: 1px 1px 1px #000000;
+  cursor: pointer;
+  transition: ease 0.5s;
+
+  :hover {
+    color: #e7a17e;
+  }
+`
