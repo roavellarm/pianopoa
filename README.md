506c835cc611cff71b4eae7fcd1baa73f0b0bb30
Rodrigo Avellar <18143403+roavellarm@users.noreply.github.com>
2022-04-09 19:12:52 -0300

    Readme updates (#6)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/d0127dd8-3d8a-482b-ba89-34bf5258f087/deploy-status)](https://app.netlify.com/sites/piano-poa/deploys)
+
 # PIANO POA
+
+[Site oficial](https://piano-poa.netlify.app/) em desenvolvimento da pianista e professora Jordana Brusa.
--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,5 @@
 export default {
   testEnvironment: 'jsdom',
-  roots: ['<rootDir>/src'],
-  verbose: true,
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
@@ -10,9 +8,4 @@ export default {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  globals: {
-    'ts-jest': {
-      diagnostics: false,
-    },
-  },
 }
