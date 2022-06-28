c0710f0b7514d9bd727337a05178249d374e006d
Gustavo <gustavo.prog40@gmail.com>
2022-06-28 15:19:46 -0300

    idem

--- src/components/FooterB/styles.ts
+++ src/components/FooterB/styles.ts
@@ -14,7 +14,7 @@ export const FooterContainerB = styled.div`
   padding: 3rem;
 
   @media screen and (max-width: 800px) {
-    height: 266px;
+    height: 275px;
   }
 `
 
@@ -65,6 +65,6 @@ export const ContainerImages = styled.div`
   display: flex;
 
   @media screen and (max-width: 700px) {
-    margin-top: 6px;
+    margin-top: 22px;
   }
 `
