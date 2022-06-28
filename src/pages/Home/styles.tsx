3de67824e60a263f554468a71e4fc6ff17d118f8
Gustavo <gustavo.prog40@gmail.com>
2022-06-28 16:12:17 -0300

    recallsssadf

--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -72,7 +72,7 @@ export const Button = styled.button`
     padding-right: 0.6rem;
     margin-top: 0;
     filter: brightness(59%);
-    margin-right: 40px;
+    margin-right: 36px;
   }
 
   @media screen and (min-width: 400px) and (max-width: 700px) {
@@ -84,7 +84,7 @@ export const Button = styled.button`
     padding-right: 0.6rem;
     margin-top: 0;
     filter: brightness(59%);
-    margin-right: 40px;
+    margin-right: 36px;
   }
 
   @media screen and (min-width: 700px) and (max-width: 800px) {
