0c1e1c003cfd218b57901b5791fb5548aff3884f
Gustavo <gustavo.prog40@gmail.com>
2022-06-29 09:09:44 -0300

    fazendo

---
+++ src/components/Menu/styled-menu.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const MenuContainer = styled.div`
+  display: none;
+
+  @media screen and (max-width: 800px) {
+    display: flex;
+    justify-content: right;
+    align-items: flex-end;
+    margin-left: 118px;
+  }
+`
